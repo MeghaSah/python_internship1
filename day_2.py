f38c4caa0356a374dcd120352281b1a3a7d352a9
MeghaSah <74900035+MeghaSah@users.noreply.github.com>
2020-11-24 12:44:51 +0545

    Add files via upload

---
+++ day_2.py
@@ -0,0 +1,15 @@
+#PROBLEM -1
+a,b,c = 2,3,5
+print(a / 10)
+print(b * 50)
+print(c + 60)
+
+#PROBLEM -2
+str="CYBER"
+print(str.replace('B', 'G'))
+
+#PROBLEM -3
+a = 10
+b = 20.0
+print(float(a))
+print(int(b))
